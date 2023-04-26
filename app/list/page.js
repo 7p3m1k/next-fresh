d10a2abf14c025baee64b2409b99f37f02e9ea35
7p3m1k <kor0885@gmail.com>
2023-04-27 02:59:34 +0900

    img 추가

--- app/list/page.js
+++ app/list/page.js
@@ -1,13 +1,16 @@
 export default function List() {
+  let items = ["Tomatoes", "Pasta", "Coconut"];
   return (
     <div>
       <h4 className="title">상품목록</h4>
-      <div className="food">
-        <h4>상품1 $40</h4>
-      </div>
-      <div className="food">
-        <h4>상품2 $40</h4>
-      </div>
+      {items.map((item, index) => {
+        return (
+          <div className="food" key={index}>
+            <img src={`/food${index}.png`} className="food-img" />
+            <h4>{item} $40</h4>
+          </div>
+        );
+      })}
     </div>
   );
 }
